52057b5857db592a53cfaa778e52fefcbc2224c3
Diego Fischer <diegof01gm@gmail.com>
2024-12-05 18:04:46 -0300

    se refacciono el codigo de ruleta aportado por julian y juan y se agrego al proyecto.

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 import { Casino } from './casino';
 import { TragamonedasEgipto } from './tragamonedasEgipto';
 import { TragamonedasAnimal } from './tragamonedasAnimal';
+import { Ruleta } from './ruleta';
 
 // Crear instancia del casino
 const casino = new Casino();
 
 // Registrar juegos
 const tragamonedasEgipto = new TragamonedasEgipto('Tragamonedas Egipto', 3, 5, 50);
-const tragamonedasAnimal = new TragamonedasAnimal('Tragamonedas Animales', 5, 10, 100);
+const tragamonedasAnimal = new TragamonedasAnimal('Tragamonedas Animales', 3, 5, 50);
+const ruleta = new Ruleta('Ruleta');
+
 casino.registrarJuego(tragamonedasEgipto);
 casino.registrarJuego(tragamonedasAnimal);
+casino.registrarJuego(ruleta);
 
 // Iniciar el casino
 casino.iniciar();
